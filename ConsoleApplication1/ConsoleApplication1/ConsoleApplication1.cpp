7cb26818818bca7aab39f4c1da505b0aa32736bc
rnrytjr2 <43197846+rnrytjr2@users.noreply.github.com>
2022-10-20 21:37:29 +0900

    스택구현

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,55 +2,102 @@
 //
 #define collection cin.tie(NULL);ios_base::sync_with_stdio(false);
 #include <iostream>
-#include <stack>
 using namespace std;
+
+class StackClass
+{
+public:
+	StackClass();
+	~StackClass();
+	void push(int _value)
+	{
+		stackcount++;
+		values[stackcount] = _value;
+	}
+	int pop()
+	{
+		if (stackcount != 0)
+		{
+			stackcount--;
+			return values[stackcount + 1];
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	int size()
+	{
+		return stackcount;
+	}
+	int empty()
+	{
+		if (stackcount != 0)
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	int top()
+	{
+		if (stackcount != 0)
+		{
+			return values[stackcount];
+		}
+		else
+		{
+			return -1;
+		}
+	}
+private:
+	int values[10000], stackcount;
+};
+
+StackClass::StackClass()
+{
+	stackcount = 0;
+}
+
+StackClass::~StackClass()
+{
+}
+
 int main()
 {
-    collection;
-    int N=0;
-    stack<int> stack;
-    string order="";
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> order;
-        if (order=="push")
-        {
-            int value=0;
-            cin >> value;
-            stack.push(value);
-        }
-        else if (order == "pop")
-        {
-            if (stack.size() != 0)
-            {
-                cout << stack.top() << '\n';
-                stack.pop();
-            }
-            else
-            {
-                cout << "-1" << '\n';
-            }
-        }
-        else if (order == "size")
-        {
-            cout << stack.size() << '\n';
-        }
-        else if (order == "empty")
-        {
-            cout << stack.empty() << '\n';
-        }
-        else if (order == "top")
-        {
-            if (stack.size() != 0)
-            {
-            cout << stack.top() << '\n';
-            }
-            else
-            {
-                cout << "-1" << '\n';
-            }
-        }
-    }
+	collection;
+	int N = 0;
+	StackClass stack;
+	string order = "";
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		order = "";
+		cin >> order;
+		if (order == "push")
+		{
+			int value = 0;
+			cin >> value;
+			stack.push(value);
+		}
+		else if (order == "pop")
+		{
+			cout << stack.pop() << '\n';
+		}
+		else if (order == "size")
+		{
+			cout << stack.size() << '\n';
+		}
+		else if (order == "empty")
+		{
+			cout << stack.empty() << '\n';
+		}
+		else if (order == "top")
+		{
+			cout << stack.top() << '\n';
+		}
+	}
 }
 
